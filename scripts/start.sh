a9620303398af6b446db1c704b1d1b82a7ba5da8
Sejal Pawar <sejal.pawar@acc.ltd>
2026-02-18 16:48:49 +0530

    update Start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-pip3 install -r requirements.txt
-nohup python3 app.py > app.log 2>&1 &
+
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-2.amazonaws.com
+
+docker pull 123456789012.dkr.ecr.us-east-2.amazonaws.com/task-manager:latest
+docker stop task-manager || true
+docker rm task-manager || true
+docker run -d -p 5000:5000 --name task-manager 123456789012.dkr.ecr.us-east-2.amazonaws.com/task-manager:latest
