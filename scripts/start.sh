d8b3ee88c4c27c544e1caa9f60f88ec77303efd2
Sejal Pawar <sejal.pawar@acc.ltd>
2026-02-18 18:10:38 +0530

    Update buildspec

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 989615686095.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region us-east-2 | \
+docker login --username AWS --password-stdin \
+989615686095.dkr.ecr.us-east-2.amazonaws.com
 
-docker pull 989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd/task-manager:latest
+docker pull 989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd:latest
+
 docker stop task-manager || true
 docker rm task-manager || true
-docker run -d -p 5000:5000 --name task-manager 989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd/task-manager:latest
+
+docker run -d -p 5000:5000 --name task-manager \
+989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd:latest
