351a42a93dbe85c256f6c542b45d5f2659f0a7b1
Sejal Pawar <sejal.pawar@acc.ltd>
2026-02-18 17:57:41 +0530

    update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 989615686095.dkr.ecr.us-east-2.amazonaws.com
 
-docker pull 123456789012.dkr.ecr.us-east-2.amazonaws.com/task-manager:latest
+docker pull 989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd/task-manager:latest
 docker stop task-manager || true
 docker rm task-manager || true
-docker run -d -p 5000:5000 --name task-manager 123456789012.dkr.ecr.us-east-2.amazonaws.com/task-manager:latest
+docker run -d -p 5000:5000 --name task-manager 989615686095.dkr.ecr.us-east-2.amazonaws.com/task-manager-cicd/task-manager:latest
